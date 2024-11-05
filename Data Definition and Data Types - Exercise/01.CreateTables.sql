bd843a0d79314e1eb88db5a3a69202e4dfc77a00
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-11-05 12:53:22 +0200

    Add files via upload

---
+++ Data Definition and Data Types - Exercise/01.CreateTables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE minions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+CREATE TABLE towns (
+    town_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
